81fca53be7dc617db104f0fac462644383524e98
Kerry Clements <kerryclements@Kerrys-Air.local.tld>
2022-03-31 19:08:02 +0100

    all changes

---
+++ php/constants.php
@@ -0,0 +1,6 @@
+<?php
+define("DB_USERNAME", "webdevcourse");
+define("DB_PASSWORD", "DKA3hO!!brJR1f6Agy");
+define("WEATHER_API_KEY", "6ca5460789ab51c52a161e27c799589");
+define("LONDON_LAT", "51.515558");
+define("LONDON_LONG", "-0.132093");
